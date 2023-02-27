b15e137baa439abcc95550ee3166a150e1673b9d
qowlsghk <baevolutiond@gmail.com>
2023-02-27 13:53:45 +0900

    no message

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ window.onload = function () {
   AOS.init();
 
   // projects json 연결
-  fetch("./assets/project.json")
+  fetch("./project.json")
     .then((res) => res.json())
     .then((result) => {
       makeList(result);
@@ -175,8 +175,8 @@ window.onload = function () {
   })
 
   // 네비 바 클릭하면 위치 이동
-  const projectsStart = document.querySelector('.projects-txt');
-  projects.addEventListener("click", function () {
-    projectsStart.scrollIntoView({ behavior: "smooth" });
-  });
+
+  projects.addEventListener('click', function () {
+    window.scrollTo({ top: 1100, behavior: 'smooth' });
+  })
 };
